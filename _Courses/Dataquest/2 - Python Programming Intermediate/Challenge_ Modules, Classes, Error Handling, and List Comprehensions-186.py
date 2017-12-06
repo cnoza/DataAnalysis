96926f6d8527f7ac267c4fa852de90e81b759175
cnoza <31512667+cnoza@users.noreply.github.com>
2017-12-06 07:00:42 +0100

    Import

--- _Courses/Dataquest/2 - Python Programming Intermediate/Challenge_ Modules, Classes, Error Handling, and List Comprehensions-186.py
+++ _Courses/Dataquest/2 - Python Programming Intermediate/Challenge_ Modules, Classes, Error Handling, and List Comprehensions-186.py
@@ -24,3 +24,33 @@ unique_games = set( [ game[2] for game in nfl_suspensions ] )
 
 print(unique_teams)
 print(unique_games)
+
+## 4. Suspension Class ##
+
+class Suspension:
+    def __init__(self, data):
+        self.name = data[0]
+        self.team = data[1]
+        self.games = data[2]
+        self.year = data[-2]
+        
+third_suspension = Suspension(nfl_suspensions[2])
+        
+
+## 5. Tweaking the Suspension Class ##
+
+class Suspension():
+    def __init__(self,row):
+        self.name = row[0]
+        self.team = row[1]
+        self.games = row[2]
+        try:
+            self.year = int(row[-2])
+        except Exception:
+            self.year = 0
+            
+    def get_year(self):
+        return self.year
+    
+missing_year = Suspension(nfl_suspensions[22])
+twenty_third_year = missing_year.get_year()
