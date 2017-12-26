f3b0b7a035afd810977b56c8ffc56b8d18ee0cfd
cnoza <31512667+cnoza@users.noreply.github.com>
2017-12-26 11:12:01 +0100

    Import

--- _Courses/[Dataquest] [11] Sql intermediate/Command line PostgreSQL-190.sh
+++ _Courses/[Dataquest] [11] Sql intermediate/Command line PostgreSQL-190.sh
@@ -6,9 +6,15 @@
 
 /home/dq$ psql
 
+	CREATE DATABASE dbName;
+
 ## 3. Special PostgreSQL commands ##
 
 /home/dq$ psql
+
+	\l -- list all available databases.
+	\dt -- list all tables in the current database.
+	\du -- list users.
 
 ## 4. Switching databases ##
 
@@ -17,15 +23,26 @@
 ## 5. Creating users ##
 
 /home/dq$ psql
+	
+	CREATE ROLE userName WITH CREATEDB LOGIN PASSWORD 'password';
 
 ## 6. Adding permissions ##
 
 /home/dq$ psql -d bank_accounts
 
+	GRANT SELECT, INSERT, UPDATE, DELETE ON tableName TO userName;
+	GRANT ALL PRIVILEGES ON tableName TO userName;
+	\dp tableName
+	
 ## 7. Removing permissions ##
 
 /home/dq$ psql -d bank_accounts
 
+	REVOKE SELECT, INSERT, UPDATE, DELETE ON tableName FROM userName;
+	REVOKE ALL PRIVILEGES ON tableName FROM userName;
+
 ## 8. Superusers ##
 
 /home/dq$ psql
+
+	CREATE ROLE userName WITH LOGIN PASSWORD 'password' SUPERUSER;
